57851cb781205f86e4927d9229d7777734cd7463
AshikShuvo <ashikshuvo1996@gmail.com>
2023-04-06 15:09:16 +0600

    logo added

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,10 +1,19 @@
+import Container from "@/app/components/HOC/Container";
+import Logo from "@/app/components/navbar/Logo";
+
 const Navbar=()=>{
     return (
         <div className="fixed w-full bg-white z-10 shadow-sm">
             <div
                 className="py-4 border-b-[1px] text-rose-500"
             >
-                I am Navbar
+                <Container>
+                    <div
+                        className="flex flex-row items-center justify-between gap-3 md:gap-0"
+                    >
+                        <Logo/>
+                    </div>
+                </Container>
             </div>
         </div>
     )
