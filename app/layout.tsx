a1bba50a0484e92983ad502b8acc0db5d269b5a9
AshikShuvo <ashikshuvo1996@gmail.com>
2023-04-06 14:46:17 +0600

    global css fix and basic nav setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import {Nunito} from "next/font/google";
-
-
-
-
+import "./globals.css"
+import Navbar from "@/app/components/navbar/Navbar";
 export const metadata = {
   title: 'Airbnb Clone',
   description: 'This is Airbnb clone',
@@ -17,7 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar/>
+        {children}
+      </body>
     </html>
   )
 }
