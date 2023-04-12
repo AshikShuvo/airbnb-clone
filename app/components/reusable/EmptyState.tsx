d638c454018391ebb95a6396ff40e702a7e0d1e3
AShikShuvo <ashikshuvo1996@gmail.com>
2023-04-12 15:10:16 +0600

    empty state added

---
+++ app/components/reusable/EmptyState.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { useRouter } from "next/navigation";
+import Heading from "@/app/components/reusable/Heading";
+import Button from "@/app/components/reusable/Button";
+
+interface EmptyStateProps {
+    title?: string;
+    subtitle?: string;
+    showReset?: boolean;
+}
+
+const EmptyState: React.FC<EmptyStateProps> = ({title = "No exact matches", subtitle = "Try changing or removing some of your filters.", showReset}) => {
+    const router = useRouter();
+
+    return (
+        <div
+            className="h-[60vh] flex flex-col gap-2 justify-center items-center"
+        >
+            <Heading
+                centered
+                title={title}
+                subTitle={subtitle}
+            />
+            <div className="w-48 mt-4">
+                {showReset && (
+                    <Button
+                        outline
+                        label="Remove all filters"
+                        onClick={() => router.push('/')}
+                    />
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default EmptyState;
